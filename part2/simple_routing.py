709e27aa1b6f2dc697d971dea3b5930b8df8e2e3
qinxx232@umn.edu <qinxx232@umn.edu>
2016-11-06 14:51:34 -0800

    Part2 Nan

--- part2/simple_routing.py
+++ part2/simple_routing.py
@@ -1,3 +1,6 @@
+# csci4211
+# project3 part2
+
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 
@@ -13,14 +16,19 @@ table = {}
 # matching rule.
 
 def _handle_PacketIn (event):
-  # check if the entry is in the table or not
-  # if it's not in the table, add an entry to the table
-  # We don't know where the destination is yet.  So, we'll just
-  # send the packet out all ports (except the one it came in on!)
-  # and hope the destination is out there somewhere. :)
-  # To send out all ports, we can use either of the special ports
-  # OFPP_FLOOD or OFPP_ALL. 
-  # if the appropriate entry is in the table, just forward the packet to that port
+  
+  # tell the switch that the future packet with dst = packet.src should automatically go event.port
+  packet = event.parsed
+  msg = of.ofp_flow_mod()
+  msg.match.dl_dst = packet.src
+  msg.actions.append(of.ofp_action_output(port = event.port))
+  event.connection.send(msg)
+  log.debug("Installing %s <-> %s" % (packet.src, packet.dst))
+  # without using table {}
+  # flood
+  msg = of.ofp_packet_out(data = event.ofp)
+  msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+  event.connection.send(msg)
 
 def launch ():
   core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
