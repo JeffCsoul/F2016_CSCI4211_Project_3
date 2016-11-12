441d9d8f23e29d1457c14804782e7a4d6cc1a935
Tianhao <liux3271@umn.edu>
2016-11-12 17:07:02 -0600

    Part5
    Part5

--- part5/part5solution.py
+++ part5/part5solution.py
@@ -13,19 +13,25 @@ from mininet.log import setLogLevel
 class AssignmentNetworks(Topo):
     def __init__(self, **opts):
         Topo.__init__(self, **opts)
-        lvl1_bw = 100000000
-        lvl2_bw = 100000
-        lvl3_bw = 100
+        lvl1_bw = 100
+        lvl2_bw = 40
+        lvl3_bw = 10
+
         lvl1_delay = '30ms'
         lvl2_delay = '20ms'
         lvl3_delay = '10ms'
+
+        #Start to build the tree here.
         c1 = self.addSwitch('c1')
+
         a1 = self.addSwitch('a1')
         a2 = self.addSwitch('a2')
+
         e1 = self.addSwitch('e1')
         e2 = self.addSwitch('e2')
         e3 = self.addSwitch('e3')
         e4 = self.addSwitch('e4')
+
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
@@ -37,10 +43,13 @@ class AssignmentNetworks(Topo):
        
         self.addLink(c1, a1, bw=lvl1_bw, delay=lvl1_delay)
         self.addLink(c1, a2, bw=lvl1_bw, delay=lvl1_delay)
+
         self.addLink(a1, e1, bw=lvl2_bw, delay=lvl2_delay)
         self.addLink(a1, e2, bw=lvl2_bw, delay=lvl2_delay)
+
         self.addLink(a2, e3, bw=lvl2_bw, delay=lvl2_delay)
         self.addLink(a2, e4, bw=lvl2_bw, delay=lvl2_delay)
+        
         self.addLink(e1, h1, bw=lvl3_bw, delay=lvl3_delay)
         self.addLink(e1, h2, bw=lvl3_bw, delay=lvl3_delay)
         self.addLink(e2, h3, bw=lvl3_bw, delay=lvl3_delay)
@@ -49,9 +58,7 @@ class AssignmentNetworks(Topo):
         self.addLink(e3, h6, bw=lvl3_bw, delay=lvl3_delay)
         self.addLink(e4, h7, bw=lvl3_bw, delay=lvl3_delay)
         self.addLink(e4, h8, bw=lvl3_bw, delay=lvl3_delay)
-        #Start to build the tree here.
-
-       
+        
  
         
 if __name__ == '__main__':
